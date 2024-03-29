823888c5655f625addade4294a8995c02e14656b
ajtollo <khesir2002@gmail.com>
2024-03-29 15:55:48 +0800

    Application.properties connected the env file

--- backend/src/main/java/com/ancientstudents/backend/BackendApplication.java
+++ backend/src/main/java/com/ancientstudents/backend/BackendApplication.java
@@ -2,10 +2,17 @@ package com.ancientstudents.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackendApplication {
-
+	
+	@Bean
+	public Dotenv dotenv() {
+        return Dotenv.configure().load();
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
